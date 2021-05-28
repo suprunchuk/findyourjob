c006e06894ba7db5a15cbf1337c634f874978986
Pasha Suprunchuk <suprunchuk@internet.ru>
2021-05-28 15:43:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar");
+const closeMenuButton = document.querySelector(".close-button");
+
+menuButton.addEventListener("click", (event) => {
+  menu.classList.toggle("is-open")
+})
+closeMenuButton.addEventListener("click", (event) => {
+  menu.classList.toggle("is-open")
+})
